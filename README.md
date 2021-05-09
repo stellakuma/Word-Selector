1a5d9c550b626f2574f454d80436e804181eee31
cyanpen <cyanpen13@gmail.com>
2021-05-09 23:33:49 +0900

    Second Upload.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Word-Selector
+Random word selector for does who memorization "english" word 
+
+## Version History 
+ > v0
+ >> First commit - this program isn't working. ****DEPRECATED****
+ >> Second commit - for someone's help, RWS now working. but text file creating is TODO.
+
+## Special Thanks
+ > Ranol¡ÙP`21
--- Random-Word-Seletor.cpp
+++ Random-Word-Seletor.cpp
@@ -2,67 +2,30 @@
 #include <cstring>
 #include <cstdlib>
 #include <ctime>
-
-cout << "********" << endl;
-            cout << stk << endl;
-            cout << rpt << endl;
-            cout << r[stk] << endl;
-            cout << a[rpt] << endl;
-            cout << "********" << endl;
+#include <vector>
 
 using namespace std;
 
-int tof(){
+int main() {
 	
-	return static_cast<int>(2*(rand()*(1.0/(RAND_MAX+1.0))));
-	
-}
+	srand(time(nullptr));
+	vector<string> i = { "Tenant","Postpone","Renovate","Archeological","Generate","Relic","Accommodate","Enterprise","Structure","Print","Correction","Issue","Mandatory","Obtainment","Fledging","Highly","Soar","Sanitation","Post","Salaried","Wage","Acquirement","Compensation","Rely on","Objective" };
+	vector<string>::iterator iter = i.begin();
 
-int main(){
-	
-	//list size
-	int ls = 25;
-	//test word list... TODO :: load list from text file
-	string a[ls] = {"Tenant","Postpone","Renovate","Archeological","Generate","Relic","Accommodate","Enterprise","Structure","Print","Correction","Issue","Mandatory","Obtainment","Fledging","Highly","Soar","Sanitation","Post","Salaried","Wage","Acquirement","Compensation","Rely on","Objective"};
-	
-	srand(time(NULL));
-    int tg = 0;
-    cout << "출력할 단어의 갯수롤 입력해주세요." << endl;
-	cin >> tg;
-	
-	int stk = 0;
-	int rpt = 0;
-	
-	//result word list
-	string r[tg] = {"a", };
-	
-    
-	while( stk < tg ){
-		cout << "A" << endl;
-		if( ls-stk <= tg ){
-		    cout << ls-stk << endl;
+	int p = 0;
+	cout << "Write down the amount of word as you want." << "\n" << flush;
+	cin >> p;
+	vector<string> r(p);
+
+	while (p --> 0) {
 		
-			if( tof() ){
-			
-				r[stk] = a[rpt];
-				stk++;
-			
-			}
-			rpt++;
+		int a = rand() % (i.size() - 1);
+		r[p] = i[a];
+		i.erase(iter+a);
 
-		}else{
-			
-			r[stk] = a[rpt];
-			rpt++;
-			stk++;
-			
-		}
 	}
 	
-	for( int i = 0; i < tg; i++ ){
-		
-		cout << r[i] << endl; 
-		
-	}
-   
+	p = r.size();
+	while ( p --> 0 ) { cout << [p] << '\n'; }cout.flush();
+
 }
