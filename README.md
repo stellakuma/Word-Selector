f92917f7f50d0d94e870a224644fdbe5cbcd29cd
cyanpen <cyanpen13@gmail.com>
2021-05-22 00:36:34 +0900

    Fourth commit

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Random word selector for does who memorization "english" word
  >> First commit - this program isn't working.  
  >> Second commit - for someone's help, RWS now working. but text file creating is TODO.  
  >> Third commit - Now can printing text file. TODO : read file from desktop and creating file at desktop.  
+ >> Fourth commit - Add reading part only. TODO : make word vector from temp string.
 
 ## Special Thanks
  > Ranol☆P`21
--- Random-Word-Seletor.cpp
+++ Random-Word-Seletor.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
+#include <string>
 #include <cstdlib>
 #include <ctime>
 #include <vector>
 #include <direct.h>
-
 
 using namespace std;
 
 int main() {
 	
 	srand(time(nullptr));
+
+	/* LIST READ PART */
+	string temp = "";
+	ifstream wlist;
+	wlist.open("c:\\WORD_TEST\\words.txt");
+	if (wlist.is_open()) {
+	
+		while (!wlist.eof()) {
+			
+			getline(wlist, temp);
+
+		}
+		wlist.close();
+
+	}
+
+	/* LIST BUILD PART */
+	//TODO :: Create vector of word using "temp" string
+
 	vector<string> i = { "Tenant","Postpone","Renovate","Archeological","Generate","Relic","Accommodate","Enterprise","Structure","Print","Correction","Issue","Mandatory","Obtainment","Fledging","Highly","Soar","Sanitation","Post","Salaried","Wage","Acquirement","Compensation","Rely on","Objective" };
 	vector<string>::iterator iter = i.begin();
 
@@ -29,6 +47,7 @@ int main() {
 
 	}
 
+	/* LIST PRINT PART */
 	_mkdir("c:\\WORD TEST");
 	ofstream print;
 	print.open("c:\\WORD TEST\\WORD TEST.txt");
